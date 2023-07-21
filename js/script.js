91fb20b5844b75fecf8e28a69854a9a2efaea43b
Pis4ni <pis4wrk@gmail.com>
2023-07-21 16:33:04 +0200

    add: script + 3 variables

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* creo una variabile con il valore "FizzBuzz" */
+let fizzBuzz = "FizzBuzz"
+/* creo una variabile con il valore "Fizz" */
+let fizz = "Fizz"
+/* creo una variabile con il valore "Buzz" */
+let buzz = "Buzz"
